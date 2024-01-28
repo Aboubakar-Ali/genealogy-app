d0b088f97d0646cddb9fb0b68fba6156720387de
Aboubakar-Ali <abdoulazizdjankado.aboubakarali@ynov.com>
2024-01-28 14:48:53 +0100

    client

--- client/src/components/FamilyTree.js
+++ client/src/components/FamilyTree.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import PersonNode from './PersonNode';
-import AddPersonForm from './AddPersonForm'; 
+import AddPersonForm from './AddPersonForm';
+import AddChildrenForm from './AddChildrenForm';
 import './FamilyTree.css';
 
 const FamilyTree = () => {
   const [family, setFamily] = useState([]);
+  const [selectedParentId, setSelectedParentId] = useState(null); // Correctement défini comme état
 
   useEffect(() => {
     axios.get('http://localhost:5000/people')
@@ -17,18 +19,28 @@ const FamilyTree = () => {
       });
   }, []);
 
-  // acces à `setFamily`
+  const onChildAdded = (newChild) => {
+    setFamily([...family, newChild]);
+  };
+
   const onPersonAdded = (newPerson) => {
     setFamily([...family, newPerson]);
+  };
+
+  const selectParent = (parentId) => {
+    setSelectedParentId(parentId); // Correctement implémentée pour définir l'ID du parent sélectionné
   };
 
   return (
     <div className="family-tree">
       <h2>Arbre Généalogique</h2>
-      <AddPersonForm onPersonAdded={onPersonAdded} /> {/* AJout formulaire*/}
+      <AddPersonForm onPersonAdded={onPersonAdded} />
       {family.map(person => (
-        <PersonNode key={person._id} person={person} />
+        <PersonNode key={person._id} person={person} onSelectParent={() => selectParent(person._id)} />
       ))}
+      {selectedParentId && (
+        <AddChildrenForm parentId={selectedParentId} onChildAdded={onChildAdded} />
+      )}
     </div>
   );
 };
